b66b0be5cdce8f25b1f60995380f6b9d75e28237
NSBum <duncan.alan@me.com>
2017-02-21 04:50:54 -0500

    import local modules

--- Contents/Server Plugin/plugin.py
+++ Contents/Server Plugin/plugin.py
@@ -5,7 +5,7 @@ import indigo
 import indigoPluginUtils
 import re
 import urllib2
-import xmltodict
+import weather, station
 
 ERROR_DEFAULT = 999.9
 
@@ -119,53 +119,3 @@ class Plugin(indigo.PluginBase):
 			return (False, valuesDict, errorsDict)
 		indigo.server.log(valuesDict['address'])
 		return (True, valuesDict)
-
-class Observation:
-	def __init__(self,doc):
-		self.data = doc['siteData']['currentConditions']
-		self.timestamp = self.data['dateTime'][0]['timeStamp']
-		self.currentConditions = CurrentConditions(self.data)
-		self.yesterdayConditions = YesterdayConditions(doc['siteData']['yesterdayConditions'])
-
-class CurrentConditions:
-	def __init__(self,xml):
-		self.xml = xml
-		self.condition = self.xml['condition']
-		self.temperature = float(self.xml.get('temperature', {}).get('#text',ERROR_DEFAULT))
-		self.tempString = "{0}°C".format(self.temperature) if self.temperature != ERROR_DEFAULT else "NA"
-
-		self.dewpoint = float(self.xml.get('dewpoint', {}).get('#text',ERROR_DEFAULT))
-		self.dewpointString = "{0}°C".format(self.dewpoint) if self.dewpoint != ERROR_DEFAULT else "NA"
-
-		self.humidity = float(self.xml.get('relativeHumidity', {}).get('#text',ERROR_DEFAULT))
-		self.pressure = float(self.xml.get('pressure', {}).get('#text',999.9))
-
-		self.visibility = float(self.xml.get('visibility', {}).get('#text',999.9))
-		self.winds = Winds(self.xml['wind'])
-
-class Winds:
-	def __init__(self,xml):
-		self.xml = xml
-		self.windSpeed = int(self.xml['speed']['#text'])
-		self.windDirection = self.xml['direction']
-
-		#	sometimes a bearing is not provided
-		#	so use an error bearing of 999.9
-		self.windBearing = float(self.xml.get('bearing', {}).get('#text',999.9))
-		self.windGust = int(self.xml.get('gust', {}).get('#text',0))
-
-class YesterdayConditions:
-	def __init__(self,xml):
-		self.xml = xml
-		temp1 = float(self.xml.get('temperature', {})[0].get('#text',999.9))
-		temp2 = float(self.xml.get('temperature', {})[1].get('#text',999.9))
-		self.yesterdayHigh = max(temp1,temp2)
-		self.yesterdayLow = min(temp1,temp2)
-		self.yesterdayPrecip = float(self.xml.get('precip', {}).get('#text',999.9))
-
-class Station:
-	def __init__(self,province,stationID):
-		self.prov = province
-		self.addr = stationID
-	def url(self):
-		return "http://dd.weather.gc.ca/citypage_weather/xml/{0}/{1}_e.xml".format(self.prov,self.addr)

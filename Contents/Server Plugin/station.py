475062eab924f721e9d46ed626b867f3ea00c12a
NSBum <duncan.alan@me.com>
2017-02-21 04:50:28 -0500

    moved Station class to separate file

---
+++ Contents/Server Plugin/station.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Station:
+	def __init__(self,province,stationID):
+		self.prov = province
+		self.addr = stationID
+	def url(self):
+		return "http://dd.weather.gc.ca/citypage_weather/xml/{0}/{1}_e.xml".format(self.prov,self.addr)
